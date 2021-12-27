9bd3daeb1a9a0e9fcff54f132b8a030371819ecc
HarshaReddyVardhan <mhvr226@gmail.com>
2021-12-28 00:03:06 +0530

    Component html changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,6 +4,7 @@ function ExpenseItem() {
       <div>27 Dec 2021</div>
       <h2>Car Bike Insurance</h2>
       <div>$149.27</div>
+      <h3>This is fixed Price <br></br>Do not bargain</h3>
     </div>
   );
 }
