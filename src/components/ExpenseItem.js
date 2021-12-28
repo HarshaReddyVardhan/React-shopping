e8d49da6d3119673b24ba054d818ebee40ccf182
HarshaReddyVardhan <mhvr226@gmail.com>
2021-12-29 00:06:07 +0530

    Added dynamic variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 12,28);
+  const expenseName = "Car Insurance";
+  const expenseAmount= 267.88; 
   return (
     <div className="expense-item">
-      <div>27 Dec 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Bike Insurance</h2>
-        <div className="expense-item__price">$149.27</div>
+        <h2>{expenseName}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
